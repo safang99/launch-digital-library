b0243fe4270ccd654d0b763da8d887dcf8828600
Stephen Fang <safang99@gmail.com>
2021-04-16 16:54:49 -0400

    fixed wizards list

--- client/src/components/WizardsList.js
+++ client/src/components/WizardsList.js
@@ -1,7 +1,12 @@
 import React from "react"
 
 const WizardsList = (props) => {
-  return "All the Wizards"
+  return (
+    <ul>
+      <li>Wizard 1</li>
+      <li>Gandalf</li>
+    </ul>
+  )
 }
 
 export default WizardsList
